19b291c05cea2f461cb629ce8a1a2d730147e7dc
Pookiloo <156447078+Pookiloo@users.noreply.github.com>
2024-09-14 21:09:08 -0400

    Update 1.0
    Currently my resume site is fully complete and can only be updated from now on to make it more complex. As for now we have a skeleton.

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ function toggleMenu() {
     const menu = document.querySelector('.navbar ul');
     menu.classList.toggle('show');
 }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("project-item");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" hover", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " hover";
+}
